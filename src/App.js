4f3e48ab7bca1926281484c2e2830aab438f752f
chrisclarkedev <cclarke8904@gmail.com>
2021-10-28 14:34:59 -0400

    created checkboxes for various requirements

--- src/App.js
+++ src/App.js
@@ -13,6 +13,53 @@ function App() {
               <i className="far fa-clipboard"></i>
             </button>
           </div>
+          <div className="form-group">
+            <label htmlFor="password-strength">Password Strength</label>
+            <input
+              type="number"
+              id="password-strength"
+              name="password-strength"
+              max="20"
+              min="10"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="uppercase-letters">Include Uppercase Letters</label>
+            <input
+              type="checkbox"
+              id="upercase-letters"
+              name="upercase-letters"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="lowercase-letters">Include Lowercase Letters</label>
+            <input
+              type="checkbox"
+              id="lowercase letters"
+              name="lowercase letters"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="include-numbers">Include Numbers</label>
+            <input
+              type="checkbox"
+              id="include-numbers"
+              name="include-numbers"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="include-symbols">Include Symbols</label>
+            <input
+              type="checkbox"
+              id="include-symbols"
+              name="include-symbols"
+            />
+          </div>
+          <button className="generator_btn">Generate</button>
         </div>
       </div>
     </div>
